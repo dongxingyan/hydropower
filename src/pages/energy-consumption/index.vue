e61606d70d6ebc2dea3d1191f7c27f8886d804e4
dongxingyan <dongxingyan3@jd.com>
2020-05-01 11:35:46 +0800

    feat:支持less

--- src/pages/energy-consumption/index.vue
+++ src/pages/energy-consumption/index.vue
@@ -1,7 +1,8 @@
 <template>
-  <div class="hello">
-      <span>{{msg}}</span>
-  <div>dhdl </div>
+  <div class="wrap">
+      <div class='header'>
+        融自能耗数据
+      </div>
   </div>
 </template>
 
@@ -15,3 +16,13 @@ export default {
     }
 };
 </script>
+
+<style lang="less" scoped>
+.wrap{
+  .header{
+    width: 100%;
+    height: 80px;
+    background-color: 'pink'
+  }
+}
+</style>
